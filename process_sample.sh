d2d58a86ab9825c74279dbd98f52502cc8262075
husttroy <troyathust@gmail.com>
2017-08-02 12:25:24 -0700

    Update the script to process the updated set of clones.

--- process_sample.sh
+++ process_sample.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # change the following directory of sampled clones to the one in your own machine
-DIR="/home/troy/research/Integration-Study/sample"
+DIR="/home/troy/research/Integration-Study/dataset/sample"
 
 i=0
 for f in $DIR/*
@@ -11,20 +11,29 @@ do
 	fName=${temp[-1]}
 	echo "File name $fName"
 	cd $f
-	echo "Renaming so.txt to $fName.txt"
-	sed -i '1ipublic class foo{' so.txt
-	echo "}" >> so.txt
-	mv so.txt "$fName.java"
+	for f1 in $f/so-*.txt
+	do	
+		temp1=(${f1//// })
+		fName1=${temp1[-1]}
+		echo "Adding class headers to $fName1"
+		sed -i '1ipublic class foo{' $fName1
+		echo "}" >> $fName1
+		ss=(${fName1//-/ })
+		num=${ss[1]}
+		num=${num%.*} # strip off .txt			
+		echo "Renaming $fName1 to $fName-$num.java"
+		mv $fName1 "$fName-$num.java"
+	done
 	for f2 in $f/gh*.txt
 	do
 		temp2=(${f2//// })
 		fName2=${temp2[-1]}
-		echo "Carving GitHub file $fName2 to carved-$fName2"
+		#echo "Carving GitHub file $fName2 to carved-$fName2"
 		#echo "File name $fName2"
 		arr=(${fName2//-/ })
-		#echo "${arr[1]}"
-		#echo "${arr[2]%.*}"
-		sed -n "${arr[1]}","${arr[2]%.*}"p $f2 > "carved-$fName2"
+		#echo "${arr[2]}"
+		#echo "${arr[3]%.*}"
+		sed -n "${arr[3]}","${arr[4]%.*}"p $f2 > "carved-$fName2"
 		sed -i '1ipublic class foo{' "carved-$fName2"
 		echo "}" >> "carved-$fName2"
 		#rename txt file to java file
