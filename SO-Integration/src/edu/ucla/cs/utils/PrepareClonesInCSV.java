726a4fcb6e005360fc4bc8415ae6bc6bdb24a3fc
husttroy <troyathust@gmail.com>
2018-08-08 19:19:19 -0700

    update the program that summarizes clone pairs in a csv file.

--- SO-Integration/src/edu/ucla/cs/utils/PrepareClonesInCSV.java
+++ SO-Integration/src/edu/ucla/cs/utils/PrepareClonesInCSV.java
@@ -4,6 +4,7 @@ import java.io.File;
 import java.io.IOException;
 import java.nio.charset.Charset;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 
 import org.apache.commons.io.FileUtils;
@@ -15,8 +16,8 @@ import edu.ucla.cs.so.diff.GumTreeDiff;
 public class PrepareClonesInCSV {
 	public static void main(String[] args)
 			throws UnsupportedOperationException, IOException {
-		String path = "/home/troy/research/Integration-Study/dataset/new-gh-with-so-links";
-		String csvPath = "/home/troy/ExampleStack/study/log/new-real-clone-sample.csv";
+		String path = "/home/troy/research/Integration-Study/dataset/clone-codes-new-sample";
+		String csvPath = "manual-inspection/scc-sample.csv";
 		
 		File csvFile = new File(csvPath);
 		if(csvFile.exists()) {
@@ -33,12 +34,13 @@ public class PrepareClonesInCSV {
 			File urlFile = null;
 			for (File file : dir.listFiles()) {
 				String fName = file.getName();
-				if (fName.startsWith("so-") && !fName.endsWith("~")) {
+				if(fName.endsWith("~")) {
+					continue;
+				} else if (fName.startsWith("so-") && fName.endsWith(".java")) {
 					soFile = file;
-				} else if (fName.startsWith("carved-gh-")
-						&& !fName.endsWith("~")) {
+				} else if (fName.startsWith("carved-gh-") && fName.endsWith(".java")) {
 					carvedGHFiles.add(file);
-				} else if (fName.equals("urls.txt")) {
+				} else if (fName.equals("urls.txt") || fName.equals("gh-urls.txt")) {
 					urlFile = file;
 				}
 			}
@@ -46,19 +48,19 @@ public class PrepareClonesInCSV {
 			if (soFile != null && !carvedGHFiles.isEmpty() && urlFile != null) {
 				String postId = soFile.getName().split("-")[1];
 				String link = "https://stackoverflow.com/questions/" + postId;
+				HashMap<Integer, String> urls = MyFileUtils.getCloneURL(urlFile);
 				for (File ghFile : carvedGHFiles) {
 					String ghFileName = ghFile.getName();
 					int ghIndex = Integer.parseInt(ghFileName.split("-")[2]);
-					List<String> urls = FileUtils.readLines(urlFile,
-							Charset.defaultCharset());
 					String url =  urls.get(ghIndex);
-					url = url.split("\t")[1];
-					url = "https://github.com/" + url;
-					GumTreeDiff differ = new GumTreeDiff(
-							soFile.getAbsolutePath(), ghFile.getAbsolutePath());
-					List<Action> edits = differ.diff();
-					MyFileUtils.appendStringToFile(groupId + "," + link + ","
-							+ url + "," + edits.size() + System.lineSeparator(), csvPath);
+					if(url != null) {
+						// ignore those that do not have urls
+						GumTreeDiff differ = new GumTreeDiff(
+								soFile.getAbsolutePath(), ghFile.getAbsolutePath());
+						List<Action> edits = differ.diff();
+						MyFileUtils.appendStringToFile(groupId + "," + link + ","
+								+ url + "," + edits.size() + System.lineSeparator(), csvPath);
+					}
 				}
 			}
 		}
